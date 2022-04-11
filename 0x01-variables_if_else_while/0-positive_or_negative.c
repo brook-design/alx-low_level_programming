04806b5177ad0ba8d7c17763e331b04da1e0dbff
brook-design <101639241+brook-design@users.noreply.github.com>
2022-04-11 08:05:32 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,12 +7,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n <= 0.0) {
-        if (n == 0.0)
+	if (n <= 0) 
+	{
+        if (n == 0)
             printf("is zero");
         else
             printf("is negative");
-    } 
+        } 
     else
         printf("is positive");
    
