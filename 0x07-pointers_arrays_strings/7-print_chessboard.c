ed38aa6227458258f3f520eddf4f1122fc33e97c
brook-design <101639241+brook-design@users.noreply.github.com>
2022-04-26 01:57:31 -0700

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: pointer to an array with 8 index places.
+ * Return: nothing.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
+	}
+
+}
