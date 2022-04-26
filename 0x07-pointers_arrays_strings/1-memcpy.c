fefcd8d1ae525d69f89a6f4c0f9f0823558d57ac
brook-design <101639241+brook-design@users.noreply.github.com>
2022-04-26 01:34:35 -0700

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: destination
+ * @src: source
+ * @n: Number of bytes
+ *
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
