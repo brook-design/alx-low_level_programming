026f212d91ccdbc52dfd6d5ce3e8ac3ba541752c
brook-design <101639241+brook-design@users.noreply.github.com>
2022-04-27 08:24:29 -0700

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _sqrt_recursion - function returns natural square root.
+ * @n: integer to be evaluated by function.
+ * Return: integer.
+ */
+
+int _pow(cur,  n);
+
+int _sqrt_recursion(int n)
+{
+
+	
+	if ( )
+	{
+		return (-1);
+	}
+}
